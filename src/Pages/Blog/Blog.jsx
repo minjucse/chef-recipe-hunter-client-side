943619edacfe3bd29f0badf8a7972ce38e2477e1
minjucse <minjucse@gmail.com>
2023-05-04 20:52:18 +0600

    blog page added

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -3,7 +3,44 @@ import React from 'react'
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='container'>
+      <div className=" row">
+        <div className='col-md-8 card mt-4 p-4'>
+          <div className="mb-4">
+            <h1 className="font-bold mb-2">
+              1. Tell us the differences between uncontrolled and controlled components.
+            </h1>
+            <p>
+              <strong>Answer</strong>: Controlled components does not maintain its internal state.
+              Controlled components data is controlled by the parent component. But, Uncontrolled componentsmaintains its internal states.
+            </p>
+          </div>
+          <div className="mb-4">
+            <h1 className="font-bold mb-2">2. How to validate React props using PropTypes
+
+            </h1>
+            <p>
+              <strong>Answer</strong>: The prop-types library makes component prop data type validation easy by providing us with ways to validate any prop types.
+            </p>
+          </div>
+          <div className="mb-4">
+            <h1 className="font-bold mb-2">
+              3. Tell us the difference between nodejs and express js
+            </h1>
+            <p>
+              <strong>Answer</strong> :Node.js is a platform for building the i/o applications which are server-side event-driven and made using JavaScript. Express.js is a framework based on Node.js which is used for building web-application using approaches and principles of Node.js event-driven architecture.
+            </p>
+          </div>
+          <div className="mb-4">
+            <h1 className="font-bold mb-2">4. What is a custom hook, and why will you create a custom hook?</h1>
+            <p>
+              <strong>Answer</strong> : Custom Hooks are a mechanism to reuse stateful logic.
+              The main reason to write a custom hook is for code reusability. 
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/Pages/Chefs/ChefDetails/ChefDetails.jsx
+++ src/Pages/Chefs/ChefDetails/ChefDetails.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { toast } from "react-toastify";
+
 
 const ChefDetails = () => {
+  const detailInfo = useLoaderData();
+  const [cart, setCart] = useState([]);
+  
+  useEffect(() => {
+    const items = JSON.parse(localStorage.getItem('shopping-cart'));
+    if (items) {
+      setCart(items);
+    }
+  }, []);
+
+  const handleAddToCart = (did) => {
+
+    let newCart = [];
+    const storedCart = localStorage.getItem('shopping-cart');
+    const shoppingCart = JSON.parse(storedCart);
+    
+    if (shoppingCart != null) {
+      const exists = shoppingCart.find(pd => pd === did);
+      
+      if (!exists) {
+
+        newCart = [...cart, did];
+        localStorage.setItem('shopping-cart', JSON.stringify(newCart));
+      }
+      else {
+        toast.error("You Have Already Aplied!");
+      }
+    }
+    else{
+      newCart = [...cart, did]
+      localStorage.setItem('shopping-cart', JSON.stringify(newCart));
+    }
+  }
   return (
     <div>ChefDetails</div>
   )
 }
 
-export default ChefDetails
+export default ChefDetails;
