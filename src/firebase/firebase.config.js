b04263f66c08361a7b0e9a9350d999077b2ee386
minjucse <minjucse@gmail.com>
2023-05-04 12:27:32 +0600

    Firebase Config error solve

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,12 +1,13 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 
+console.log('test local'+import.meta.env.VITE_PASS);
 const firebaseConfig = {
-    apiKey: import.meta.env.vite_apiKey,
-    authDomain: import.meta.env.vite_authDomain,
-    projectId: import.meta.env.vite_projectId,
-    storageBucket: import.meta.env.vite_storageBucket,
-    messagingSenderId: import.meta.env.vite_messagingSenderId,
-    appId: import.meta.env.vite_appId
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.vite_VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID
   };
   
   // Initialize Firebase
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect } from 'react';
+import React, { useState, useEffect, createContext } from 'react';
 
 import {
     getAuth,
